63c4cefaca94ba3e9feac996dee7651904a7cb7f
maedeh zarvandi <maedeh97@maedehs-MacBook-Pro.local>
2021-08-06 00:20:27 +0430

    first commit

---
+++ architectures/ResNet.py
@@ -0,0 +1,183 @@
+'''ResNet in PyTorch.
+
+For Pre-activation ResNet, see 'preact_resnet.py'.
+
+Reference:
+[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+    Deep Residual Learning for Image Recognition. arXiv:1512.03385
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion *
+                               planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, opt):
+        super(ResNet, self).__init__()
+        num_classes = opt['num_classes']
+        block = BasicBlock
+        num_blocks = opt['num_blocks']
+
+        self.in_planes = 64
+
+        num_stages = 4
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+        self.all_feat_names = ['conv'+str(s+1) for s in range(num_stages)] + ['classifier',]
+
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def _parse_out_keys_arg(self, out_feat_keys):
+
+        # By default return the features of the last layer / module.
+        out_feat_keys = [self.all_feat_names[-1],] if out_feat_keys is None else out_feat_keys
+
+        if len(out_feat_keys) == 0:
+            raise ValueError('Empty list of output feature keys.')
+        for f, key in enumerate(out_feat_keys):
+            if key not in self.all_feat_names:
+                raise ValueError('Feature with name {0} does not exist. Existing features: {1}.'.format(key, self.all_feat_names))
+            elif key in out_feat_keys[:f]:
+                raise ValueError('Duplicate output feature key: {0}.'.format(key))
+
+        # Find the highest output feature in `out_feat_keys
+        max_out_feat = max([self.all_feat_names.index(key) for key in out_feat_keys])
+
+        return out_feat_keys, max_out_feat
+
+
+    def forward(self, x, out_feat_keys=None):
+        out_feat_keys, max_out_feat = self._parse_out_keys_arg(out_feat_keys)
+        out_feats = [None] * len(out_feat_keys)
+
+        out = F.relu(self.bn1(self.conv1(x)))
+
+        out = self.layer1(out)
+        if 'conv1' in out_feat_keys:
+            out_feats[out_feat_keys.index('conv1')] = out
+
+        out = self.layer2(out)
+        if 'conv2' in out_feat_keys:
+            out_feats[out_feat_keys.index('conv2')] = out
+
+        out = self.layer3(out)
+        if 'conv3' in out_feat_keys:
+            out_feats[out_feat_keys.index('conv3')] = out
+
+        out = self.layer4(out)
+        if 'conv4' in out_feat_keys:
+            out_feats[out_feat_keys.index('conv4')] = out
+
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        if 'classifier' in out_feat_keys:
+            out_feats[out_feat_keys.index('classifier')] = out
+        
+        out_feats = out_feats[0] if len(out_feats)==1 else out_feats
+        return out_feats
+
+
+def create_model(opt):
+    return ResNet(opt)
+
+def ResNet18():
+    return ResNet(BasicBlock, [2, 2, 2, 2])
+
+
+def ResNet34():
+    return ResNet(BasicBlock, [3, 4, 6, 3])
+
+
+def ResNet50():
+    return ResNet(Bottleneck, [3, 4, 6, 3])
+
+
+def ResNet101():
+    return ResNet(Bottleneck, [3, 4, 23, 3])
+
+
+def ResNet152():
+    return ResNet(Bottleneck, [3, 8, 36, 3])
+
+
+def test():
+    net = ResNet18()
+    y = net(torch.randn(1, 3, 32, 32))
+    print(y.size())
+
+# test()
